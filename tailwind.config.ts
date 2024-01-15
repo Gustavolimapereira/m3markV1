6c93d666cf5e61afda4de71b8c6e49c7133d0fbf
gustavolimapereira <gu.lim@hotmail.com.br>
2024-01-14 23:21:54 -0300

    update site matheus

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        customColor: '#102129',
+        customColorCircle: '#703619',
+      },
     },
   },
   plugins: [],
