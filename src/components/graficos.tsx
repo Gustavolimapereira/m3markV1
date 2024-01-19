7fa435bcb11ee0ebee0fe444633394bc5b3210ae
Matheus <matheus.mjs@hotmail.com>
2024-01-19 19:25:44 -0300

    Correção grafico

--- src/components/graficos.tsx
+++ src/components/graficos.tsx
@@ -1,52 +1,52 @@
 export default function Graficos() {
   return (
     <div className=" w-full h-full flex justify-center items-center">
-      <div className="w-3/4 h-40 bg-customColorCard py-6 px-10 relative space-y-2 rounded-xl">
-        <div className="flex space-x-1 justify-start items-center">
+      <div className="w-3/4 h-40 bg-customColorCard py-6 px-6 relative space-y-2 rounded-xl">
+        <div className="flex space-x-5 justify-start items-center">
           <div
-            className=" bg-customColorGray rounded-full h-2.5"
-            style={{ width: '8%' }}
+            className=" bg-customColorGray rounded-full h-2"
+            style={{ width: '15%' }}
           ></div>
           <div
-            className=" bg-customColorWaterBlue rounded-full h-2.5"
-            style={{ width: '60%' }}
+            className=" bg-customColorWaterBlue rounded-full h-2"
+            style={{ width: '50%' }}
           ></div>
-          <p className="text-white pl-2">80%</p>
+          <p className="text-white">80%</p>
         </div>
-        <div className="flex space-x-1 justify-start items-center">
+        <div className="flex space-x-5 justify-start items-center">
           <div
-            className=" bg-customColorGray rounded-full h-2.5"
-            style={{ width: '8%' }}
+            className=" bg-customColorGray rounded-full h-2"
+            style={{ width: '15%' }}
           ></div>
           <div
-            className=" bg-customColorBlue rounded-full h-2.5"
-            style={{ width: '30%' }}
+            className=" bg-customColorBlue rounded-full h-2"
+            style={{ width: '15%' }}
           ></div>
-          <p className="text-white pl-2">30%</p>
-        </div>
-
-        <div className="flex space-x-1 justify-start items-center">
-          <div
-            className=" bg-customColorGray rounded-full h-2.5"
-            style={{ width: '8%' }}
-          ></div>
-          <div
-            className=" bg-white rounded-full h-2.5"
-            style={{ width: '70%' }}
-          ></div>
-          <p className="text-white pl-2">100%</p>
+          <p className="text-white">30%</p>
         </div>
 
-        <div className="flex space-x-1 justify-start items-center">
+        <div className="flex space-x-5 justify-start items-center">
           <div
-            className=" bg-customColorGray rounded-full h-2.5"
-            style={{ width: '8%' }}
+            className=" bg-customColorGray rounded-full h-2"
+            style={{ width: '15%' }}
           ></div>
           <div
-            className=" bg-customColorGreen rounded-full h-2.5"
-            style={{ width: '55%' }}
+            className=" bg-white rounded-full h-2"
+            style={{ width: '65%' }}
           ></div>
-          <p className="text-white pl-2">75%</p>
+          <p className="text-white">100%</p>
+        </div>
+
+        <div className="flex space-x-5 justify-start items-center">
+          <div
+            className=" bg-customColorGray rounded-full h-2"
+            style={{ width: '15%' }}
+          ></div>
+          <div
+            className=" bg-customColorGreen rounded-full h-2"
+            style={{ width: '45%' }}
+          ></div>
+          <p className="text-white">75%</p>
         </div>
       </div>
     </div>
